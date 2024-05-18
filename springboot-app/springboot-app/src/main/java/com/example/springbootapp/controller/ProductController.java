228c2d96df54c461417d5c489bbfc09553e9059a
Francisco Macedo <macedolb53@gmail.com>
2024-05-18 17:54:02 +0200

    second step

--- springboot-app/springboot-app/src/main/java/com/example/springbootapp/controller/ProductController.java
+++ springboot-app/springboot-app/src/main/java/com/example/springbootapp/controller/ProductController.java
@@ -1,6 +1,14 @@
 package com.example.springbootapp.controller;
+import com.example.springbootapp.datatranferobject.ProductRecorDto;
+import com.example.springbootapp.models.ProductModel;
 import com.example.springbootapp.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -12,4 +20,13 @@ public class ProductController {
 
     @Autowired
     ProductRepository productRepository;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecorDto productRecorDto){
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecorDto, productModel);//O que vai ser convertido e o tipo no qual sera convertido
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+
+
+    }
 }
--- springboot-app/springboot-app/src/main/java/com/example/springbootapp/datatranferobject/ProductRecorDto.java
+++ springboot-app/springboot-app/src/main/java/com/example/springbootapp/datatranferobject/ProductRecorDto.java
@@ -1,8 +1,11 @@
 package com.example.springbootapp.datatranferobject;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
-public record ProductRecorDto(String name, BigDecimal value) {
-    
+public record ProductRecorDto(@NotBlank String name, @NotNull BigDecimal value) {
+
 
 }
