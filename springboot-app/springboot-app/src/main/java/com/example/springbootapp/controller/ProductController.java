08c2712744f469f1603ae23de3e702bfeca48fb0
Francisco Macedo <macedolb53@gmail.com>
2024-05-18 19:48:46 +0200

    update metodo

--- springboot-app/springboot-app/src/main/java/com/example/springbootapp/controller/ProductController.java
+++ springboot-app/springboot-app/src/main/java/com/example/springbootapp/controller/ProductController.java
@@ -30,13 +30,11 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
     }
 
-
     @GetMapping("/products")
     //Este metodo tem como funcaao mostrar todos os produtos salvos na nossa base de dados
     public ResponseEntity<List<ProductModel>> getAllProducts(){
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
     }
-
     @GetMapping("/products/{id}")
     //Este metodo nos permite pegar um unico produto na nossa base de dados
     public ResponseEntity<Object> getOneProduct(@PathVariable(value="id") UUID id){
@@ -46,6 +44,16 @@ public class ProductController {
         }
         return ResponseEntity.status(HttpStatus.OK).body(productO.get());
     }
-
+    @PutMapping("/products/{id}")
+    //Este metodo nos permite actualizar os nosso produtos pelo id do produto
+    public ResponseEntity<Object> updateProduct(@PathVariable(value="id") UUID id, @RequestBody @Valid ProductRecorDto productRecordDto) {
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if(productO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        var productModel = productO.get();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+    }
 
 }
