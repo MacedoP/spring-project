5c4e4cb736694814518b860c83d89580f0818bce
Francisco Macedo <macedolb53@gmail.com>
2024-05-18 15:52:42 +0200

    start

---
+++ springboot-app/springboot-app/src/main/java/com/example/springbootapp/models/ProductModel.java
@@ -0,0 +1,17 @@
+package com.example.springbootapp.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "TB PRODUCTS") //VIA DE CONEXAO COM A NOSSA TABELA DENTRO DA NOSSA BASE DE DADOS
+
+public class ProductModel implements Serializable{
+    /*
+    NESTA CLASSE CRIAMOS O MODELO DE NOSSOS PRODUTOS
+    * */
+
+
+}
